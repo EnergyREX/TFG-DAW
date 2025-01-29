fdbb3da4b262533597c36b9ade7472c9bc6ca96e
EnergyREX <energy.rex.2003@gmail.com>
2025-01-29 14:23:18 +0100

    feat: registration system

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    function index() {
+        return view('register');
+    }
+
+    function store(Request $request) {
+        // Validate Data
+        $validated = request()->validate([
+            'dni' => ['required'],
+            'name' => ['required'],
+            'surname' => ['required'],
+            'address' => ['required'],
+            'phone_number' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed']
+        ]);
+
+        // Store in database
+        $user = User::create($validated);
+
+        Auth::login($user);
+        // Redirection
+        return redirect('/login');
+    }
+}
