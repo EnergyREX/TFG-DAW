f9d9adf27851a2421e5f4e6b9b33d5d6fa042814
EnergyREX <energy.rex.2003@gmail.com>
2025-01-30 14:12:34 +0100

    upd: migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -25,7 +26,7 @@ return new class extends Migration
             $table->string('specialization')->nullable();
             $table->string('availability')->nullable();
             $table->string('state')->nullable();
-            $table->integer('role_id');
+            $table->foreignIdFor(Role::class);
             $table->rememberToken();
             $table->timestamps();
         });
