8f890abadcc97e06df2a3fc2a3dedb8e525cebd9
EnergyREX <energy.rex.2003@gmail.com>
2025-01-30 14:30:37 +0100

    feat: index and show

--- app/Http/Controllers/TreatmentController.php
+++ app/Http/Controllers/TreatmentController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Treatment;
 use Illuminate\Http\Request;
 
 class TreatmentController extends Controller
 {
     // Manage Treatment inserts, update, delete and index.
+    function index() {
+        return view('treatments');
+    }
+
+    function show($id) {
+        if (!Treatment::find($id)) {
+            return response('Not found', 404)
+            ->header('Content-Type', 'text/plain');
+        }
+
+        return Treatment::find($id);
+    }
 }
