a99cec1c3481fc89af3695c3d0b549558ee51929
EnergyREX <energy.rex.2003@gmail.com>
2025-01-30 14:29:36 +0100

    feat: index and show

--- app/Http/Controllers/MedicalRecordController.php
+++ app/Http/Controllers/MedicalRecordController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MedicalRecord;
 use Illuminate\Http\Request;
 
 class MedicalRecordController extends Controller
 {
     // Manage MedicalRecords inserts, update, delete and index.
+    function index() {
+        return view('medical_records');
+    }
+
+    function show($id) {
+        if (!MedicalRecord::find($id)) {
+            return response('Not found', 404)
+            ->header('Content-Type', 'text/plain');
+        }
+
+        return MedicalRecord::find($id);
+    }
 }
