13390ffe403f7a80ca20fe26987351c891cf3563
EnergyREX <energy.rex.2003@gmail.com>
2025-01-30 14:30:30 +0100

    feat: index and show

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
     // Manage Role inserts, update, delete and index.
+    function index() {
+        return view('roles');
+    }
+
+    function show($id) {
+        if (!Role::find($id)) {
+            return response('Not found', 404)
+            ->header('Content-Type', 'text/plain');
+        }
+
+        return Role::find($id);
+    }
 }
