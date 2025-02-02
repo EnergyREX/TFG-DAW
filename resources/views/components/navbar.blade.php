86df6a7b047fe19067add8a4b7220c84e7a205db
EnergyREX <energy.rex.2003@gmail.com>
2025-02-02 20:42:16 +0100

    add: temporary navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="/">Index</a></li>
+        <li><a href="/appointments">Appointments</a></li>
+        <li><a href="/inventories">Inventories</a></li>
+        <li><a href="/medicalrecords">Medical Records</a></li>
+        <li><a href="/treatments">Treatments</a></li>
+        <li><a href="/roles">Roles</a></li>
+    </ul>
+</nav>
