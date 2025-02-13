bbbf5a160764f1095c47d031739431bd103ae0d1
EnergyREX <energy.rex.2003@gmail.com>
2025-02-13 23:55:28 +0100

    feat: hasPermission function

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Role extends Model
 {
     /** @use HasFactory<\Database\Factories\RoleFactory> */
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name'
@@ -20,5 +21,9 @@ class Role extends Model
 
     public function permission() {
         return $this->belongsToMany(Permission::class, 'roles_permissions');
+    }
+
+    public function hasPermission($permission) {
+        return $this->permission->contains('name', $permission);
     }
 }
