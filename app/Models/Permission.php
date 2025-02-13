2e08790ec8db93302a251a83d53c73ca99311a23
EnergyREX <energy.rex.2003@gmail.com>
2025-02-13 23:52:47 +0100

    add: permission model

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Permission extends Model
 {
+    use HasFactory, Notifiable;
     protected $fillable = ['name'];
+    
+
+    public function roles() {
+        return $this->belongsToMany(Role::class, 'roles_permissions');
+    }
 }
