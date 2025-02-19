0353e500761efa66b624423288949ed4556e8db6
EnergyREX <energy.rex.2003@gmail.com>
2025-02-19 11:09:22 +0100

    experiment (testing AJAX requests)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+const xhr = new XMLHttpRequest();
+const URI = window.location.href
+
+xhr.onreadystatechange = function() {
+  if(this.readyState == 4 && this.status == 200) {
+    console.log('READY')
+    document.getElementById("content").innerHTML = xhr.responseText;
+    console.log(JSON.parse(xhr.responseText))
+  }
+}
+
+xhr.open("GET", `${URI}appointments/api/all`, true)
+xhr.send()
