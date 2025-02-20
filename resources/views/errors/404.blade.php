cb8da6e56dfe005e1980bf1e256fc6560b1d0f66
EnergyREX <energy.rex.2003@gmail.com>
2025-02-20 14:22:45 +0100

    feat: 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<x-layouts.error>
+    <x-slot:title>
+        Page not Found
+    </x-slot>
+<main class="error">
+    <div class="error__card">
+        <h1>404</h1>
+        <h4>Page not found</h4>
+        <div>
+            <p>Looks like that you tried to access to an unexistent page</p>
+            <p>Return to <a href="/">Dashboard</a>
+        </div>
+    </div>
+</main>
+</x-layouts.error>
